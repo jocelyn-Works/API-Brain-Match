70be84f2ac3a12210937e3b42e6125a60bca6553
jocelyn <grelierjocelyn@gmail.com>
2025-07-25 14:52:08 +0200

    add user /me

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -48,14 +48,37 @@ module.exports.signIn = async (req, res) => {
     const token = createToken(user._id);
 
     res.status(200).json({
-      token,
-      user
+      token
     });
 
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
+module.exports.getProfile = async (req, res) => {
+  try {
+    const user = await UserModel.findById(res.locals.userId).select('-password');
+
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur introuvable" });
+    }
+
+    const baseUrl = `${req.protocol}://${req.get('host')}`; // ex: http://localhost:5000
+
+    const userWithPictureUrl = {
+      ...user._doc,
+      picture: user.picture
+        ? `${baseUrl}/${user.picture.replace(/^\.?\/*/, '')}`
+        : null,
+    };
+
+    res.status(200).json(userWithPictureUrl);
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur", error: err.message });
+  }
+};
+
 
 
 // Deconnexion
