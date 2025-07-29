f2d007829eb3efded6a84ec428bf3e03967a907d
Zakuross <zakariya.belmokhtar@le-campus-numerique.fr>
2025-07-29 15:48:53 +0200

    index+admin up

--- app.js
+++ app.js
@@ -25,7 +25,11 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // FRONT ADMIN
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "admin.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/admin", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "admin.html"));
 });
 
 // DB connection
